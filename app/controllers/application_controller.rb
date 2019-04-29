99d21c6abecac40c6b000721affb558d8840d846
Kyle  Beckstead <kyle21beckstead@gmail.com>
2019-04-29 18:05:25 +0000

    Automatically backed up by Learn

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,41 @@
+require_relative '../../config/environment'
+class ApplicationController < Sinatra::Base
+  configure do
+    set :views, Proc.new { File.join(root, "../views/") }
+    enable :sessions unless test?
+    set :session_secret, "secret"
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  post '/login' do
+    @user = User.find_by(username: params[:username], password: params[:password])
+
+    if @user
+      session[:user_id] = @user.id
+      redirect '/account'
+    end
+
+    erb :error
+  end
+
+  get '/account' do
+    @user = User.find_by_id(session[:user_id])
+
+    if @user
+      erb :account
+    else
+      erb :error
+    end
+
+  end
+
+  get '/logout' do
+    session.clear
+    redirect '/'
+  end
+
+
+end
